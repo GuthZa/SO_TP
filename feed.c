76eac4c82c257b031a6c27739a4b53173c6ef986
João Claro <joao9.claro99@gmail.com>
2024-10-30 20:38:05 +0000

    Initial commit

---
+++ feed.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <signal.h>
+
+int main(int argc, char *argv[])
+{
+    char username[100];
+    // Remove o buffer em stdout, de modo a mostrar as mensagens assim que recebe
+    // E não agurdar para que o buffer esteja "cheio" com mensagens
+    setbuf(stdout, NULL);
+
+    printf("\n Hello user!\n\n\tPlease enter your username: ");
+    scanf("%s", username);
+
+    // Envia nome para manager para analise se já existe
+
+    // if (/*existe*/)
+    // {
+    //     printf("\nDo be welcome, what would you like to do?\nEnter h for help.\n");
+    // }
+    // else
+    // {
+    //     printf("\nPlease, choose an username that's not associated with another user.\n");
+    // }
+
+    char choice[50];
+    while (1)
+    {
+        printf("\n%s $ ", username);
+        scanf("%s", choice);
+        if (strcmp(choice, "exit") == 0)
+        {
+            printf("Goodbye\n");
+            exit(EXIT_SUCCESS);
+        }
+    }
+
+    exit(EXIT_SUCCESS);
+}
---
+++ manager.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <signal.h>
+#include <stdbool.h>
+
+#define MAX_USERS 10
+#define MAX_TOPICOS 20
+#define MAX_TOPICO_NOME 20
+#define MAX_MSG_PERSIST 5
+
+bool checkIfExistsInArray(char *array[], char *str, int array_size);
+
+int main(int argc, char *argv[])
+{
+    setbuf(stdout, NULL);
+
+    char users[MAX_USERS][100];
+    char topicos[MAX_TOPICOS][MAX_TOPICO_NOME];
+    int current_users = 0;
+    int current_topicos = 0;
+
+    // Vars for debugging ----------------------------------------------------------------------------------
+    char new_user[] = "Wisura";
+    char new_topico[] = "futebol";
+
+    // Vars for debugging ----------------------------------------------------------------------------------
+
+    /* ========================== CHECK FOR MAX USERS AND DUPLICATE USERS ================================= */
+    // Recebe user de user
+    if (current_users >= MAX_USERS)
+    {
+        // Warns the user there's no room, that they should try later
+        printf("\nMax users active reached\n");
+    }
+    else
+    {
+        if (checkIfExistsInArray(users, new_user, current_users))
+        {
+            // Sends an answer back to FEED that there's already a user logged in
+            printf("\nDuplicated user.\n");
+        }
+        users[current_users++];
+        printf("\n New user logged in, welcome %s!\n", new_user);
+    }
+
+    /* ========================== CHECK FOR MAX TOPICS AND DUPLICATE TOPICS ================================= */
+    // Recebe user de user
+    if (current_topicos >= MAX_TOPICOS)
+    {
+        // Warns the user there's no room, that they should try later
+        printf("\nMax topics active reached\n");
+    }
+    else
+    {
+        if (checkIfExistsInArray(topicos, new_topico, current_topicos))
+            printf("\nThat topic already exists.\n");
+        topicos[current_topicos++];
+        printf("\n New topic added, %s!\n", new_topico);
+    }
+
+    exit(EXIT_SUCCESS);
+}
+
+bool checkIfExistsInArray(char *array[], char *str, int array_size)
+{
+    for (int i = 0; i < array_size; i++)
+        if (strcmp(array[i], str))
+            return false;
+    return true;
+}
