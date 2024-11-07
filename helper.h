3198ae234539e706a6df1f6d326a865d3fccf48a
João Claro <joao9.claro99@gmail.com>
2024-11-07 18:57:10 +0000

    Add response struct
    To synthesize responses from the manager

--- helper.h
+++ helper.h
@@ -1,55 +1,63 @@
 #define DICT_H
 
-#include <unistd.h>
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <fcntl.h>
-#include <limits.h>
 #include <sys/types.h>
+#include <unistd.h>
 #include <sys/stat.h>
-#include <ctype.h>
+#include <signal.h>
+#include <errno.h>
 
 // paths for pipe files
 #define MANAGER_PIPE "/tmp/manager_pipe"
 #define FEED_PIPE "/tmp/feed_%d_pipe"
 
 // Max size of a message, if needed to be saved in memory
-#define MAX_MSG_SIZE 300
+#define MSG_MAX_SIZE 300
 // Max size of the name of a topic
-#define MAX_TOPIC_SIZE 20
+#define TOPIC_MAX_SIZE 20
 // Max size of the user name
-#define MAX_USER_SIZE 100
+#define USER_MAX_SIZE 100
 
 // Max users per topic
-#define MAX_USERS 10
+#define TOPIC_MAX_USERS 10
 
-#define MAX_PERSIST_MSG 5
-
-typedef struct
+typedef struct user
 {
     pid_t pid;
     char *name;
 } user;
 
-typedef struct
+typedef struct dataMSG
 {
-    char topic[MAX_TOPIC_SIZE];
+    char topic[TOPIC_MAX_SIZE];
     int msg_size;
-    char user[MAX_USER_SIZE];
+    char user[USER_MAX_SIZE];
     int time; // Time until being deleted
     char *msg;
 } dataMSG;
 
+// To use by the manager, to send messages to the user
+typedef struct response
+{
+    char topic[TOPIC_MAX_SIZE];
+    int msg_size;
+    char *msg;
+} response;
+
+// Action by the user to send messages
 typedef struct
 {
     pid_t pid_user;
     dataMSG msg;
 } message;
 
+// Ping by user to subscribe to topic
 typedef struct
 {
     pid_t pid_user;
-    char user[MAX_USER_SIZE];
-    char topic[MAX_TOPIC_SIZE];
+    char user[USER_MAX_SIZE];
+    char topic[TOPIC_MAX_SIZE];
 } subscribe;
